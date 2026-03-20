f371fe045c681897fd52910e0970eb24ab678bfe
JoaoSilva17405 <estgv17405@alunos.estgv.ipv.pt>
2026-03-20 15:42:49 +0000

    Delete Projeto/src/Bridge directory
    Update trabalho local

--- Projeto/src/Bridge/src/bridge/ConsoleDestino.java
+++
@@ -1,9 +0,0 @@
-package bridge;
-
-public class ConsoleDestino implements LogDestino {
-
-    @Override
-    public void escrever(String mensagemFormatada) {
-        System.out.println(mensagemFormatada);
-    }
-}
